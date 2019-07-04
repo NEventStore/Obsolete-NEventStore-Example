fe238619a11a7fc77d9456b08b6f49b7fabd6ed9
najlot <najlot@users.noreply.github.com>
2019-07-04 21:37:51 +0200

    NEventStore Version 6.0.0

--- NEventStore.Example/AuthorizationPipelineHook.cs
+++ NEventStore.Example/AuthorizationPipelineHook.cs
@@ -1,36 +1,45 @@
 namespace NEventStore.Example
 {
-    using System;
-    using EventStore;
+	using System;
 
-    public class AuthorizationPipelineHook : IPipelineHook
-    {
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+	public class AuthorizationPipelineHook : IPipelineHook
+	{
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 
-        public Commit Select(Commit committed)
-        {
-            // return null if the user isn't authorized to see this commit
-            return committed;
-        }
+		public ICommit Select(ICommit committed)
+		{
+			// return null if the user isn't authorized to see this commit
+			return committed;
+		}
 
-        public bool PreCommit(Commit attempt)
-        {
-            // Can easily do logging or other such activities here
-            return true; // true == allow commit to continue, false = stop.
-        }
+		public bool PreCommit(CommitAttempt attempt)
+		{
+			// Can easily do logging or other such activities here
+			return true; // true == allow commit to continue, false = stop.
+		}
 
-        public void PostCommit(Commit committed)
-        {
-            // anything to do after the commit has been persisted.
-        }
+		public void PostCommit(ICommit committed)
+		{
+			// anything to do after the commit has been persisted.
+		}
 
-        protected virtual void Dispose(bool disposing)
-        {
-            // no op
-        }
-    }
+		public void OnDeleteStream(string bucketId, string streamId)
+		{
+			// anything to do while a stream gets deleted
+		}
+
+		public void OnPurge(string bucketId)
+		{
+			// anything to do while a stream gets purged
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			// no op
+		}
+	}
 }
